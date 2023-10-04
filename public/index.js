b1d7cb305dcbafbca156db9b8c515506e7b0917f
Ilya Tsakunov <ilyatsakunov@seznam.cz>
2023-10-04 13:37:48 +0200

    Add query and answer to output

--- public/index.js
+++ public/index.js
@@ -43,15 +43,20 @@ function sendAjaxQueryDB() {
     $('.socket').show();
     $('.wrap-response h3').html("")
     $('.wrap-response p').html("")
+    $('.wrap-response h3').eq(1).html("")
+    $('.wrap-response p').eq(1).html("")
     $.ajax({
         method: "POST",
         url: "/api/query_database",
         data: { question: $('.searchTerm').val() }
     })
-    .done(function( msg ) {
-        console.log(msg)
+    .done(function( data ) {
+        console.log(data)
+
         $('.socket').hide();
         $('.wrap-response h3').html("Odpověď z databáze:")
-        $('.wrap-response p').html(msg)
+        $('.wrap-response p').html(data.answer)
+        $('.wrap-response h3').eq(1).html("SQL dotaz:")
+        $('.wrap-response p').eq(1).html(data.query)
     });
 }
